9745044d7f070a2040db161a1d462955b2fc22de
cjm <522637210@qq.com>
2023-03-03 12:45:38 +0800

    first commit

---
+++ src/main/java/gitTest01.java
@@ -0,0 +1,10 @@
+/**
+ * @author ChenJiaMing
+ * @create 2023-03-03 12:41
+ */
+public class gitTest01 {
+    public static void main(String[] args) {
+        System.out.println("git-test01");
+        System.out.println("git-test02");
+    }
+}

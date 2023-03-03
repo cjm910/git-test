7aa3f0e19b913f483c5207c17c5ffc29382767f0
cjm <522637210@qq.com>
2023-03-03 13:50:21 +0800

    Merge branch 'hot-fix'
    # Conflicts:
    #	src/main/java/gitTest01.java

--- src/main/java/gitTest01.java
+++ src/main/java/gitTest01.java
@@ -5,16 +5,7 @@
 public class gitTest01 {
     public static void main(String[] args) {
         System.out.println("master01");
-
-
-
-
-
-
-
         System.out.println("hot-fix01");
-
-
 
     }
 }

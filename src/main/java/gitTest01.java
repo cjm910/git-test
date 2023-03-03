3036e9d00c09b8d641962a43ab5f2b532ea148f0
cjm <522637210@qq.com>
2023-03-03 13:16:10 +0800

    hot-fix commit01

--- src/main/java/gitTest01.java
+++ src/main/java/gitTest01.java
@@ -4,6 +4,8 @@
  */
 public class gitTest01 {
     public static void main(String[] args) {
+        System.out.println("master01");
+        System.out.println("master02");
         System.out.println("hot-fix01");
         System.out.println("hot-fix02");
     }

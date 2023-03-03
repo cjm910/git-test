46a6a40a7d7864ca53a7255fbcad4c55cc8c4ba2
cjm <522637210@qq.com>
2023-03-03 15:17:40 +0800

    push2

--- src/main/java/gitTest01.java
+++ src/main/java/gitTest01.java
@@ -4,8 +4,6 @@
  */
 public class gitTest01 {
     public static void main(String[] args) {
-        System.out.println("master01");
-        System.out.println("hot-fix01");
         System.out.println("111111");
         System.out.println("222222");
     }

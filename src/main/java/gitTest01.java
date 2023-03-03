fb3f5bc6b49b9203d3aae2ebd7ecdc2a7975b9c0
cjm <522637210@qq.com>
2023-03-03 13:05:12 +0800

    third commit

--- src/main/java/gitTest01.java
+++ src/main/java/gitTest01.java
@@ -4,7 +4,7 @@
  */
 public class gitTest01 {
     public static void main(String[] args) {
-        System.out.println("hot-fix01");
-        System.out.println("hot-fix02");
+        System.out.println("master01");
+        System.out.println("master0");
     }
 }
